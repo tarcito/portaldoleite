4b94b93f60aec10546075e150c63b57d9c4c2b92
tarcito <tarcito.lua@gmail.com>
2015-11-22 17:40:26 -0300

    adicionado disciplinas e temas para ES1, TC, EDA e PLP

--- app/Global.java
+++ app/Global.java
@@ -58,6 +58,53 @@ public class Global extends GlobalSettings {
 		si1.addTema(new Tema("Minitestes"));
 		si1.addTema(new Tema("Projeto"));
 		dao.persist(si1);
+
+		Disciplina es1 = new Disciplina("Engenharia de Software 1");
+		es1.addTema(new Tema("Introdução à Engenharia de Software"));
+		es1.addTema(new Tema("Métodos Formais"));
+		es1.addTema(new Tema("Verificação e Validação"));
+		es1.addTema(new Tema("Evolução de Software"));
+		dao.persist(es1);
+
+		Disciplina tc = new Disciplina("Teoria da Computação");
+		tc.addTema(new Tema("Linguagens Regulares e Autômatos Finitos"));
+		tc.addTema(new Tema("Linguagens Livre-de-Contexto e Autômatos de Pilha"));
+		tc.addTema(new Tema("Máquina de Turing"));
+		tc.addTema(new Tema("Decidibilidade"));
+		tc.addTema(new Tema("Redução"));
+		dao.persist(tc);
+
+		Disciplina eda = new Disciplina("Estrutura de Dados e Algoritmos");
+		eda.addTema(new Tema("Análize de Algoritmos Iterativos"));
+		eda.addTema(new Tema("Análize de Algoritmos Recursivos"));
+		eda.addTema(new Tema("Ordenação Por Comparação - Iterativos"));
+		eda.addTema(new Tema("Ordenação Por Comparação - Recursivos"));
+		eda.addTema(new Tema("Ordenação em Tempo Linear"));
+		eda.addTema(new Tema("TAD - Vetor, Pilha e Fila"));
+		eda.addTema(new Tema("Listas Encadeadas - Iterativas"));
+		eda.addTema(new Tema("Listas Encadeadas - Recursivas"));
+		eda.addTema(new Tema("Árvore Binária de Pesquisa - BST"));
+		eda.addTema(new Tema("Heaps"));
+		eda.addTema(new Tema("Skip Lists"));
+		eda.addTema(new Tema("Tabelas Hash"));
+		eda.addTema(new Tema("Árvores AVL"));
+		eda.addTema(new Tema("Árvores Splay"));
+		eda.addTema(new Tema("Árvores PV"));
+		eda.addTema(new Tema("Árvores B"));
+		dao.persist(eda);
+
+		Disciplina plp = new Disciplina("Paradigmas de Linguagens de Programação");
+		plp.addTema(new Tema("Valores e Tipos"));
+		plp.addTema(new Tema("Variáveis e Comandos"));
+		plp.addTema(new Tema("Abstração e Mecanismos de Passagens de Parametros"));
+		plp.addTema(new Tema("Paradigma Imperativo"));
+		plp.addTema(new Tema("Paradigma Funcional"));
+		plp.addTema(new Tema("Paradigma Orientado a Objetos"));
+		plp.addTema(new Tema("Programação Orientada a Aspectos"));
+		plp.addTema(new Tema("Programação Orientado a Modelos"));
+		plp.addTema(new Tema("Programação Concorrente"));
+		dao.persist(plp);
+
 		dao.flush();
 	}
 }
